57ddb209161197b77d632793c0f338b2fab27c80
unjr <43352970+unjr@users.noreply.github.com>
2019-01-22 05:03:59 +0100

    Add files via upload

--- circuits_bipolaires.py
+++ circuits_bipolaires.py
@@ -8,58 +8,110 @@ Created on Tue Jan  8 18:50:22 2019
 import numpy as np
 from matplotlib.pyplot import *
 from math import *
+import sympy as sp
+sp.init_printing()
 
-class CircuitBipolaire():           # un objet de la classe CircuitBipolaire
-    def __init__(self,impedance):   # est muni de la grandeur "impédance"
-        self.impedance = impedance
+class CircuitBipolaire():
+    def __init__(self,valeur):
+        if isinstance(valeur,str):
+            valeur = sp.Symbol(valeur)
+        self.valeur = valeur
         
     def __repr__(self):
         return str(self.impedance)
-
-class CircuitElementaire(CircuitBipolaire):         # un circuit élémentaire hérite
-    def __init__(self,impedance):                   # du circuit bipolaire et peut
-        CircuitBipolaire.__init__(self,impedance)   # être placé en série (méthode add)
-                                                    # ou en parallèle (méthode or),
-    def __add__(self, autre_circuit):               # chaque méthode calcule l'impédance équivalente
-        impedance_totale = self.impedance + autre_circuit.impedance
-        circuit_serie = CircuitElementaire(impedance_totale)
+    
+    def __add__(self, autre_circuit):
+        circuit_serie = Serie(self,autre_circuit)
         return circuit_serie
     
     def __or__(self, autre_circuit):
-        impedance_totale = (self.impedance * autre_circuit.impedance)/(self.impedance + autre_circuit.impedance)
-        circuit_parallele = CircuitElementaire(impedance_totale)
+        circuit_parallele = Parallele(self,autre_circuit)
         return circuit_parallele
-    
-class Composant(CircuitElementaire,list):           # on définit une classe Composant
-    def __init__(self,impedance):                   # même si ça n'apporte pas grand chose pour
-        CircuitElementaire.__init__(self,impedance) # l'instant
+
+class Serie(CircuitBipolaire):
+    def __init__(self,circuit1,circuit2):
+        self.circuit1 = circuit1
+        self.circuit2 = circuit2
         
+    def get_impedance(self,omega):
+        return self.circuit1.get_impedance(omega)+self.circuit2.get_impedance(omega)
+    
+    def trace(self,x0,y0):
+        xf1,yf1 = self.circuit1.trace(x0,y0)
+        xf2,yf2 = self.circuit2.trace(xf1,y0)
+        return xf2,yf2
+
+class Parallele(CircuitBipolaire):
+    def __init__(self,circuit1,circuit2):
+        self.circuit1 = circuit1
+        self.circuit2 = circuit2
+
+    def get_impedance(self,omega):
+        return (self.circuit1.get_impedance(omega) * self.circuit2.get_impedance(omega))/(self.circuit1.get_impedance(omega) + self.circuit2.get_impedance(omega))
+    
+    def trace(self,x0,y0):
+        xf1,yf1 = self.circuit1.trace(x0,y0)
+        xf2,yf2 = self.circuit2.trace(x0,yf1+0.5)
+        plot([x0,x0],[y0,yf1+0.5],'k')
+        plot([xf1,xf1],[y0,yf1+0.5],'k')
+        plot([xf1,xf2],[yf1+0.5,yf1+0.5],'k')
+        return max(xf1,xf2),(yf2)
+
 # On définit ensuite une classe par type de composant
 # pour lesquelles, on définit les impédances correspondantes
 class Resistance(Composant):
-    def __init__(self,R):
-        Composant.__init__(self,R)
+    def get_impedance(self,omega):
+        return self.valeur
+
+    def trace(self,x0,y0): 
+        length=1; height=0.5
+        plot([x0,x0+length/4],[y0,y0],'k')
+        plot([x0+length/4,x0+length*3/4],[y0-height/2,y0-height/2],'k')
+        plot([x0+length/4,x0+length*3/4],[y0+height/2,y0+height/2],'k')
+        plot([x0+length/4,x0+length/4],[y0-height/2,y0+height/2],'k')
+        plot([x0+length*3/4,x0+length*3/4],[y0-height/2,y0+height/2],'k')
+        plot([x0+length*3/4,x0+length],[y0,y0],'k')
+        text(x0+length/2-0.1,y0,self.valeur)
+        return x0+length,y0+height/2
 
 class Inductance(Composant):
-    def __init__(self,L,w):
-        Composant.__init__(self,1j*L*w)
-
+    def get_impedance(self,omega):
+        if isinstance(self.valeur,sp.Symbol):
+            return self.valeur*sp.I*omega
+        else:
+            return self.valeur*1j*omega
+        
+    def trace(self,x0,y0): 
+        length=1; height=0.5; xlin = np.linspace(x0+length/4,x0+length*3/4,200)
+        plot([x0,x0+length/4],[y0,y0],'k')
+        plot(xlin,y0+np.sin(xlin*(20*np.pi)+np.pi)*height/2,'k')
+        plot(xlin,y0+np.sin(xlin*(20*np.pi))*height/2,'k')
+        plot([x0+length*3/4,x0+length],[y0,y0],'k')
+        return x0+length,y0+height/2
+    
 class Capacite(Composant):
-    def __init__(self,C,w):
-        Composant.__init__(self,1/(1j*C*w))
+    def get_impedance(self,omega):
+        if isinstance(self.valeur,sp.Symbol):
+            return 1/(self.valeur*sp.I*omega)
+        else:
+            return 1/(self.valeur*1j*omega)
+    
+    def trace(self,x0,y0): 
+        length=1; height=0.5
+        plot([x0,x0+length/3],[y0,y0],'k')
+        plot([x0+length/3,x0+length/3],[y0-height/2,y0+height/2],'k')
+        plot([x0+length*2/3,x0+length*2/3],[y0-height/2,y0+height/2],'k')
+        plot([x0+length*2/3,x0+length],[y0,y0],'k')
+        return x0+length,y0+height/2
         
 
-
 #### TEST ###
-f = 1; w = 2*pi*f
+f = 1; omega = sp.Symbol('omega') #omega = 2*pi*f
 
-R1 = Resistance(1)
-R2 = Resistance(1)
-C1 = Capacite(1,w)
-C2 = Capacite(1,w)
-L1 = Inductance(1,w) 
+R1 = Resistance('R_1')
+R2 = Resistance('R_2')
+C1 = Capacite('C_1')
+C2 = Capacite('C_2')
+L1 = Inductance('L_1') 
 
-circuit_final = ((R1 + (L1 | C1)) | C2) + R2
-
-print(circuit_final)
-
+circuit_final = ((R1+(C1|L1))|C2) + R2; circuit_final.trace(0,0)
